fd395f23bf5ebd6c461403600fbefb55d09ad2c5
Diego Pagini <diegopaginidev@gmail.com>
2021-01-10 09:05:49 -0300

    modal listo

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Video } from 'src/app/models/youtube.models';
 import { YoutubeService } from '../../services/youtube.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-home',
@@ -14,6 +15,28 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
     this.youtubeService.getVideos().subscribe((resp) => {
       this.videos.push(...resp);
+    });
+  }
+
+  showVideo(video: Video) {
+    Swal.fire({
+      html: `
+      <h4>${video.title}</h4>
+      <hr>
+      <iframe 
+        width="100%" 
+        height="315" 
+        src="https://www.youtube.com/embed/${video.resourceId.videoId}" 
+        frameborder="0" 
+        allow="accelerometer; 
+        autoplay; 
+        clipboard-write; 
+        encrypted-media; 
+        gyroscope; 
+        picture-in-picture" 
+        allowfullscreen>
+      </iframe>
+      `,
     });
   }
 }
